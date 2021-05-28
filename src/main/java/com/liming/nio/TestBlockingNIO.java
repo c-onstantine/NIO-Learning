bc393a8ab629c4afd00cc8d23cd4720b3c9c606a
ltf <ltf@qq.com>
2021-05-28 17:50:43 +0800

    完结

--- src/main/java/com/liming/nio/TestBlockingNIO.java
+++ src/main/java/com/liming/nio/TestBlockingNIO.java
@@ -66,7 +66,7 @@ public class TestBlockingNIO {
         //1. 获取通道
         ServerSocketChannel ssChannel = ServerSocketChannel.open();
 
-        FileChannel outChannel = FileChannel.open(Paths.get("5.jpg"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        FileChannel outChannel = FileChannel.open(Paths.get("src/main/resources/5.jpg"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
 
         //2. 绑定连接
         ssChannel.bind(new InetSocketAddress(9898));
